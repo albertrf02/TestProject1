ab26a85343f4fce14411ae794746d3b99af70c67
Albert Rocas <albertrocas02@gmail.com>
2023-11-29 16:34:53 +0100

    dades

--- mvc/src/views/dades.php
+++ mvc/src/views/dades.php
@@ -12,77 +12,84 @@ session_start();
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="../../JS/validarDades.js"></script>
+    <script src="script.js"></script>
 </head>
 
 <body>
 
     <?php
     // Check if the user is identified
-    ?>
-    <h2>Inscription Listing</h2>
-    <table id="inscriptionTable">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nom</th>
-                <th>Cognoms</th>
-                <th>Data de Naixement</th>
-                <th>Carrer</th>
-                <th>Numero</th>
-                <th>Ciutat</th>
-                <th>Codi Postal</th>
-                <th>Resguard Path</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($inscriptionsData as $inscription): ?>
+    if (isset($_SESSION['identified']) && $_SESSION['identified'] === true) {
+        ?>
+        <h2>Inscription Listing</h2>
+        <table id="inscriptionTable">
+            <thead>
                 <tr>
-                    <td>
-                        <?= $inscription['Id'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['Nom'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['Cognoms'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['DataNaixement'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['Carrer'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['Numero'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['Ciutat'] ?>
-                    </td>
-                    <td>
-                        <?= $inscription['CodiPostal'] ?>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Cognoms</th>
+                    <th>Data de Naixement</th>
+                    <th>Carrer</th>
+                    <th>Numero</th>
+                    <th>Ciutat</th>
+                    <th>Codi Postal</th>
+                    <th>Resguard Path</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($inscriptionsData as $inscription): ?>
+                    <tr>
+                        <td>
+                            <?= $inscription['Id'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['Nom'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['Cognoms'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['DataNaixement'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['Carrer'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['Numero'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['Ciutat'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['CodiPostal'] ?>
+                        </td>
+                        <td>
+                            <?= $inscription['ResguardPath'] ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
-    <script>
-        $(document).ready(function () {
-            $('#inscriptionTable').DataTable({
-                // DataTables configuration options
+        <script>
+            $(document).ready(function () {
+                $('#inscriptionTable').DataTable({
+                    // DataTables configuration options
+                });
             });
-        });
-    </script>
+        </script>
 
-    <?php
-    // Form is visible if the user is not identified
-    ?>
-    <form id="accessCodeForm" method="POST">
-        <label for="accessCode">Enter Access Code:</label>
-        <input type="password" id="accessCode" name="accessCode" required>
-        <button type="submit">Submit</button>
-    </form>
-    <?php
+        <?php
+    } else {
+        // Form is visible if the user is not identified
+        ?>
+        <form id="accessCodeForm" method="POST">
+            <label for="accessCode">Enter Access Code:</label>
+            <input type="password" id="accessCode" name="accessCode" required>
+            <button type="submit">Submit</button>
+        </form>
+        <?php
+    }
     ?>
 
 
